8e79a0e5891a21658489bf5a2a1c805829e126e5
Alexis Ivan <alexis.ivan.mt@gmail.com>
2023-05-11 22:27:11 -0600

    Added morgan

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",(req,res,next) => {
     res.status(200);
     res.send("Bienvenido al servidor");
-})
+});
 
-app.listen(3000, ()=>{
+app.get("/:name", (req, res, next) => {
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola, " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...")
 });
